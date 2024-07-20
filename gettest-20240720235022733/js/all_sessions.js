b64b27d051dd68cd185a6999d094d6160413cd09
MauroVelasco <mauro.velasco@gmail.com>
2024-07-20 23:50:30 +0000

    deploy: f24f17a16501ac5ad991b84da36d1d27ec57a2f0

---
+++ gettest-20240720235022733/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FFA900',
+name: 'Active Users',
+data: [
+  [1721519423000,3],[1721519424000,3],[1721519425000,4],[1721519426000,4],[1721519427000,3],[1721519428000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
