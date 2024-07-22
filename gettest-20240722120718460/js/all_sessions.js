ef96bf0a7cb5333c2c368f6b6d3674dd078b1780
maurovelasco <mauro.velasco@gmail.com>
2024-07-22 12:07:28 +0000

    deploy: 78de2e5fa5bc3381fd6a187c64d8440b82508ca3

---
+++ gettest-20240722120718460/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FFA900',
+name: 'Active Users',
+data: [
+  [1721650039000,2],[1721650040000,4],[1721650041000,3],[1721650042000,2],[1721650043000,5],[1721650044000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
